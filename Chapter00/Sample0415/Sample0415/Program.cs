5236142d558e72c5d350b9691a9d500cad0cbe30
infosys <infosys@C4223>
2026-04-16 10:56:04 +0900

    プロジェクト ファイルを追加します。

---
+++ Chapter00/Sample0415/Sample0415/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Globalization;
+using System.Security;
+
+namespace Sample0415
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+            //格納
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("array[" + i + "] =");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            {
+                //集計
+                int sum = 0;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    sum += array[i];
+
+                }
+
+                Console.WriteLine();
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.Write("arrya[" + i + "]:");
+
+                    astOut(array[i]);
+
+                }
+                Console.WriteLine("合計値:" + array.Where(n=>n%2 ==0).Sum());
+            }
+            void astOut(int num)
+            {
+                for (int j = 0; j < num; j++)
+                {
+                    Console.Write("*");
+                }Console.WriteLine();
+            }
+            
+        }
+    }
+}
