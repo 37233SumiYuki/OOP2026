8f9a7c551db2e60faeaa37459c0ba37e618102aa
優希 鷲見 <oibc37233@ojs.ac.jp>
2026-04-20 11:43:03 +0900

    P30コード2-3

---
+++ Chapter02/距離換算プログラム/DistanceConverter/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.Design;
+
+namespace DistanceConverter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            if (args.Length >= 1 && args[0] == "-tom")
+            {
+                for (int feet = 1; feet <= 10; feet++)
+                {
+                    double meters = FeetToMeter(feet);
+                    Console.WriteLine($"{feet}ft = {meters:0.0000} m");
+                }
+            }
+            else
+            {
+                for (int meters = 1; meters <= 10; meters++)
+                {
+                    double feet = MeterToFeet(meters);
+                    Console.WriteLine($"{meters}m = {feet:0.0000} ft");
+                }
+            }
+        }
+        static double FeetToMeter(int feet)
+        {
+            return feet * 0.3048;
+        }
+        static double MeterToFeet(int meters)
+        {
+            return meters / 0.3048;
+        }
+    }
+}
