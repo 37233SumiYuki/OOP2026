14ef743f087d2cfc4400d289d4007dc83226e45c
優希 鷲見 <oibc37233@ojs.ac.jp>
2026-05-13 10:32:53 +0900

    途中

---
+++ Chapter02/演習問題/Exercise01/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            var songs = new List<Song>();
+
+            Console.WriteLine("*****曲の登録*****");
+            Console.Write("曲名:");
+            string? title = Console.ReadLine();
+
+            Console.Write("アーティスト名:");
+            string? ArtistName = Console.ReadLine();
+
+            Console.Write("演奏時間:");
+            int length = int.Parse(Console.ReadLine());
+
+            var song = new Song(title, ArtistName, length);
+
+                songs.Add(song);
+ 
+            PrintSongs(songs);
+        }
+        private static void PrintSongs(List<Song> songs) {
+
+            foreach (var song in songs) {
+                var min = song.Length / 60;
+                var sec = song.Length % 60;
+                Console.WriteLine($" 曲名:{song.Title}\r\n アーティスト名:{song.ArtistName}\r\n 演奏時間:{min:00}:{sec:00}");
+            }
+        }
+    }
+}
---
+++ Chapter02/演習問題/Exercise01/Song.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    public class Song {
+        //タイトル
+        public string Title { get; set; } = string.Empty;
+        //商品カテゴリー
+        public string ArtistName { get; set; } = string.Empty;
+        //演奏時間
+        public int Length { get; set; }
+        //コンストラクター
+        public Song(String title, string artistName, int length) {
+            Title = title;
+            ArtistName = artistName;
+            Length = length;
+           
+
+
+        }
+    }
+}
