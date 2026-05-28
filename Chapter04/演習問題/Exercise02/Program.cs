cff4811f52cf5d6b6f720c750dfd450590f33ce4
優希 鷲見 <oibc37233@ojs.ac.jp>
2026-05-28 09:21:41 +0900

    完成

--- Chapter04/演習問題/Exercise02/Program.cs
+++ Chapter04/演習問題/Exercise02/Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Runtime.InteropServices;
+
 namespace Exercise02 {
     internal class Program {
         static void Main(string[] args) {
@@ -14,29 +16,52 @@ namespace Exercise02 {
             //if-else文を使用
             var line = Console.ReadLine();
             int num = int.Parse(line);
-            if (num > 0) {
+            if (num < 0) {
                 Console.WriteLine(num);
 
-            } else if (num <= 0 || num >= 100) {
+            } else if (num < 100) {
                 Console.WriteLine(num * 2);
 
-            } else if (num >= 100 || num <= 500) {
+            } else if (num < 500) {
                 Console.WriteLine(num * 3);
-            }
-            if (num <= 500) {
+            } else
                 Console.WriteLine(num);
-            }
         }
 
         private static void Exercise2() {
             //switch文を使用
+            var line = Console.ReadLine();
+            int num = int.Parse(line);
+            switch (num) {
+                case < 0:
+                    Console.WriteLine(num);
+                    break;
+                case < 100:
+                    Console.WriteLine(num*2);
+                    break;
+                case < 500:
+                    Console.WriteLine(num*3);
+                    break;
+                default:
+                    Console.WriteLine(num);
+                    break;
+            }
 
         }
 
         private static void Exercise3() {
             //switch式を使用
-
+            var line = Console.ReadLine();
+            var num = int.Parse(line);
+            var text = num switch {
+                < 0 =>  num,
+                < 100 =>num * 2,
+                < 500 =>num * 3,
+                _ => num
+            };
+            Console.WriteLine(text);
 
         }
     }
 }
+    
