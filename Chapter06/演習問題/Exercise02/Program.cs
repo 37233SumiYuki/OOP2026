3b250d00e9772af3755252708766ff3b91c707ea
優希 鷲見 <oibc37233@ojs.ac.jp>
2026-06-10 10:29:05 +0900

    6－2完成

---
+++ Chapter06/演習問題/Exercise02/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.Write("整数文字列:");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out int number)) {
+                var s1 = $"{number:#,0}";
+                Console.WriteLine(s1);
+            } else {
+                Console.WriteLine("int型ではありません。");
+            }
+
+
+        }
+    }
+}
