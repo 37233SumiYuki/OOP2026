4fb639339d5f4892b1c51fbaf631d6120ab86e37
優希 鷲見 <oibc37233@ojs.ac.jp>
2026-06-10 09:57:46 +0900

    6-1完成

---
+++ Chapter06/演習問題/Exercise01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.Write("1文字目:");
+            var str1 = Console.ReadLine();
+            Console.Write("2文字目:");
+            var str2 = Console.ReadLine();
+
+            var cultureInfo = new CultureInfo("ja-JP");
+
+            if (String.Compare(str1, str2, cultureInfo,
+                CompareOptions.IgnoreWidth |
+                CompareOptions.IgnoreKanaType) == 0) {
+                Console.WriteLine("等しい");
+            } else {
+                Console.WriteLine("等しくない");
+            }
+        }
+    }
+}
