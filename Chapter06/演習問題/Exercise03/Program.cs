87bbea4f6b8d19bb3a29b7deb42a79f549bbdf37
優希 鷲見 <oibc37233@ojs.ac.jp>
2026-06-11 11:16:07 +0900

    Exercise6

--- Chapter06/演習問題/Exercise03/Program.cs
+++ Chapter06/演習問題/Exercise03/Program.cs
@@ -55,17 +55,15 @@ namespace Exercise03 {
         private static void Exercise6(string text) {
             var str = text.ToLower().Replace(" ","");
 
-            //var alphDicCount = Enumerable.Range('a', 26).
-            //    ToDictionary(num => ((char)num).ToString(),num => 0);
+            var alphDicCount = Enumerable.Range('a', 26).
+                ToDictionary(num => ((char)num),num => 0);
 
-            var dict = new SortedDictionary<char, int>();
+            //var dict = new SortedDictionary<char, int>();
+
             foreach (var c in str) {
-                if (dict.ContainsKey(c))
-                    dict[c]++;
-                else
-                    dict[c] = 1;
+                alphDicCount[c]++;
             }
-            foreach(var word in dict) {
+            foreach(var word in alphDicCount) {
                 Console.WriteLine(word.Key + ":" + word.Value);
             }
 
