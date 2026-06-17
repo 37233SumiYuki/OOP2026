f6b528472212aadd6e0ad08591f21e9a58ad0a4e
優希 鷲見 <oibc37233@ojs.ac.jp>
2026-06-17 10:25:44 +0900

    6-4未完成

---
+++ Chapter06/文字列の連結と分割/Section05/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+
+namespace Section05 {
+    internal class Program {
+        static void Main(string[] args) {
+            var sb = new StringBuilder();
+            foreach (var word in GetWords()) {
+                sb.Append(word);
+            }
+            
+            Console.WriteLine(sb);
+        }
+
+        private static IEnumerable <string> GetWords() {
+            return ["Orange", "Lemon", "Strawberry"];
+        }
+    }
+}
---
+++ Chapter06/演習問題/Exercise04/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics.Tracing;
+using System.Linq;
+
+namespace Exercise04 {
+    internal class Program {
+        static void Main(string[] args) {
+            var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
+            //;で分割
+            var word = line.Split(';');
+            //=で分割
+
+            foreach (var word2 in word) {
+            var eqword = line.Split('=');
+                Console.WriteLine($"{ToJapanese(eqword[0])}")
+
+
+            static string ToJapanese(string key) {
+                return key switch {
+                    "Novelist" => "作家",
+                    "BestWork" => "代表作",
+                    "Born" => "誕生年",
+                    _ => "引数keyは、正しい値ではありません"
+                };
+                //古い書き方
+                //switch (key) {
+                //    case "Novelist":　
+                //        return "作家";
+                //    case "BestWork":
+                //        return "代表作";
+                //    case "Born":
+                //        return "誕生年";
+                //    default:
+                //        return "引数keyは、正しい値ではありません";
+                //}
+            }
+        }
+    }
+}
