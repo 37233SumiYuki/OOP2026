ff3a517b31223c5b4ab63ca28f4afce9d2517398
優希 鷲見 <oibc37233@ojs.ac.jp>
2026-06-22 10:31:14 +0900

    7-2-5完成

--- Chapter07/演習問題/Exercise02/Program.cs
+++ Chapter07/演習問題/Exercise02/Program.cs
@@ -64,7 +64,10 @@ namespace Exercise02 {
         }
 
         private static void Exercise5(List<Book> books) {
-            
+            var book = books.Where(b => b.Price <= 4000).Max(b => b.Pages);
+            Console.WriteLine(book);
+
+
         }
 
         private static void Exercise6(List<Book> books) {
