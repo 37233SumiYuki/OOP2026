8d90ceae04d8e6b9810c36ed2ce1f85fb25b8597
優希 鷲見 <oibc37233@ojs.ac.jp>
2026-06-22 12:05:23 +0900

    設問２_提出

--- TestProject/Test01_02/Program.cs
+++ TestProject/Test01_02/Program.cs
@@ -61,6 +61,8 @@
         //問題１　合計値を表示（式形式で記述せよ）
         //　　　　出力結果【618】
         private static void Exercise01(int[] numbers) {
+            var count = numbers.Sum();
+            Console.WriteLine(count);
 
         }
 
@@ -68,15 +70,17 @@
         //問題２　偶数の最大値を表示（式形式で記述せよ）
         //　　　　出力結果【94】
         private static void Exercise02(int[] numbers) {
-
+           // var count = numbers.Max(n >= n % 2 == 0);
+           // Console.WriteLine(count);
         }
 
 
         //問題３　昇順に並べて表示（遅延実行とする）
         //　　　　出力結果【12 14 17 20 31 35 40 48 53 76 87 91 94】
-        private static void Exercise03(int[] numbers) {
+        //private static void Exercise03(int[] numbers) {
+        //    var count = numbers.OrderBy(numbers).
             
-        }
+       
 
         //問題４　10以上50以下の数字のみを表示（即時実行でも可とする）
         //　　　　出力結果【12 14 20 40 35 31 17 48】
@@ -86,10 +90,13 @@
 
         //問題５　Countメソッドを使い、小文字の'n'が含まれている都市名がいくつあるかカウントして結果を表示
         //　　　　出力結果【5】
-        private static void Exercise05(List<string> cities) {
-      
+        //private static void Exercise05(List<string> cities) {
+        //    var count = cities.Count(s => s.Contains('n'));
+        //    foreach(var c in count)
+        //    Console.WriteLine(c);
+
+
         
-        }
 
         //問題６　全都市数
         //　　　　出力結果【8】
