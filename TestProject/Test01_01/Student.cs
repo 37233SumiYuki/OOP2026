336ce89d5dd5c80ee3c9836cd91837704239c5bd
優希 鷲見 <oibc37233@ojs.ac.jp>
2026-06-22 11:26:14 +0900

    設問１_Studentクラス完成

--- TestProject/Test01_01/Student.cs
+++ TestProject/Test01_01/Student.cs
@@ -1,7 +1,15 @@
-﻿namespace Test01_01 {
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Test01_01 {
     public class Student {
-        
+            
+            public string Name { get; set; } = string.Empty;
+            
+            public string Subject { get; set; } = string.Empty;
 
-
+            public int Score { get; set; }
+        }
     }
-}
