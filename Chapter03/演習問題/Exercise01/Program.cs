06ca7a3453370a4257c354384dcc5719b80e9b38
優希 鷲見 <oibc37233@ojs.ac.jp>
2026-06-22 09:57:50 +0900

    7-2-2完成

--- Chapter03/演習問題/Exercise01/Program.cs
+++ Chapter03/演習問題/Exercise01/Program.cs
@@ -3,7 +3,7 @@ namespace Exercise01 {
     internal class Program {
         static void Main(string[] args) {
             var numbers = new List<int> { 12, 87, 94, 14, 53, 20, 40, 35, 76, 91, 31, 17, 48 };
-
+            #region 各演習の呼び出し (ドライバ)
             // 3.1.1
             Exercise1(numbers);
             Console.WriteLine("-----");
@@ -18,24 +18,44 @@ namespace Exercise01 {
 
             // 3.1.4
             Exercise4(numbers);
+            #endregion
         }
 
         private static void Exercise1(List<int> numbers) {
-           
-            var exists =numbers.Exists( n => n % 8 == 0 || n % 9 == 0);
-            if(exists = 
+            Console.WriteLine("---3.1.1---");
+            var exists = numbers.Exists(n => n % 8 == 0 || n % 9 == 0);
+            if (exists) {
+                Console.WriteLine("存在しています");
+            } else {
+                Console.WriteLine("存在していません");
+            }
         }
 
         private static void Exercise2(List<int> numbers) {
-            
+            Console.WriteLine("---3.1.2---");
+            numbers.ForEach(n => Console.WriteLine(n / 2.0));
+            //foreach (var n in numbers) {
+            //    Console.WriteLine(n / 2.0);
+            //}
         }
 
         private static void Exercise3(List<int> numbers) {
-            
+            Console.WriteLine("---3.1.3---");
+            numbers.Where(n => 50 <= n).ToList().ForEach(n => Console.WriteLine(n));
+            //foreach (var n in numbers.Where(n => 50 <= n)) {
+            //    Console.WriteLine(n);
+            //}
         }
 
         private static void Exercise4(List<int> numbers) {
-            
+            Console.WriteLine("---3.1.4---");
+            var query = numbers
+                .Select(n => n * 2)
+                .ToList();
+            query.ForEach(n => Console.WriteLine(n));
+            //foreach (var s in query) {
+            //    Console.WriteLine(s);
+            //}
         }
     }
 }
--- Chapter03/演習問題/Exercise02/Program.cs
+++ Chapter03/演習問題/Exercise02/Program.cs
@@ -1,7 +1,65 @@
-﻿namespace Exercise02 {
+﻿
+
+namespace Exercise02 {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+            var cities = new List<string> {
+                "Tokyo", "New Delhi", "Bangkok", "London",
+                "Paris", "Berlin", "Canberra", "Hong Kong",
+            };
+
+            Console.WriteLine("***** 3.2.1 *****");
+            Exercise2_1(cities);
+            Console.WriteLine();
+
+            Console.WriteLine("***** 3.2.2 *****");
+            Exercise2_2(cities);
+            Console.WriteLine();
+
+            Console.WriteLine("***** 3.2.3 *****");
+            Exercise2_3(cities);
+            Console.WriteLine();
+
+            Console.WriteLine("***** 3.2.4 *****");
+            Exercise2_4(cities);
+            Console.WriteLine();
+        }
+
+        private static void Exercise2_1(List<string> cities) {
+            //できたらGitのコメント「問題3.2.1完成」
+            Console.Write("都市：");
+            var name = Console.ReadLine();
+            var index = cities.FindIndex(s => s.Equals(name));
+            Console.WriteLine(index);
+        }
+
+        private static void Exercise2_2(List<string> cities) {
+            //できたらGitのコメント「問題3.2.2完成」
+            var count = cities.Count(s => s.Contains('o'));
+            Console.WriteLine(count);
+        }
+
+        private static void Exercise2_3(List<string> cities) {
+            //できたらGitのコメント「問題3.2.3完成」
+            var query = cities
+                .Where(s => s.Contains('o'))
+                .ToArray();
+            foreach (var s in query) {
+                Console.WriteLine(s);
+            }
+        }
+
+        private static void Exercise2_4(List<string> cities) {
+            //できたらGitのコメント「問題3.2.4完成」
+            var query = cities
+                .Where(s => s.Contains("B"))
+                .Select(s => new { s, s.Length });
+
+            foreach (var data in query) {
+                Console.WriteLine(data.s + ":" + data.Length + "文字");
+            }
         }
     }
 }
+
+
